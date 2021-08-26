802fa4ef3e9cbfb34a670ec786b4fec6141c3512
David Thomas <david.thomas1986@gmail.com>
2021-08-26 23:43:56 +0100

    added connection to mongodb

--- core.js
+++ core.js
@@ -1,23 +1,52 @@
 const { MongoClient } = require("mongodb");
 
-// Connection URI
-const uri =
-"mongodb access link here";
+async function main(){
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    // Connect the client to the server
+
+  const uri =
+  "db connection string goes here";
+  
+
+  const client = new MongoClient(uri);
+
+  try{
     await client.connect();
 
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
+    await  listDatabases(client);
+
+  } catch (e){
+    console.error(e);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+
+main().catch(console.error);
+
+
+
+async function findRecipeByName(client, nameOfRecipe){
+  const result = await client.db("new_recipe_set").collection("new_recipe_set").findOne({name: nameOfRecipe})
+
+  if (result){
+    console.log(`Found Recipe in the collection with the name '${nameOfRecipe}'`);
+    console.log(result);
+  }else{
+    console.log(`No Recipe Found!`)
+  }
+}
+
+ async function createListing(client, newListing){
+  const result = await client.db("new_recipe_set").collection("new_recipe_set").insertOne(newListing);
+
+  console.log(`New Recipe created with thew following id: ${result.insertedId}`);
+
+}
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
