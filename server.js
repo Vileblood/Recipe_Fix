12e1c94a8c9f9b64b0f4094016c5637b6b69792a
David Thomas <david.thomas1986@gmail.com>
2021-09-02 21:44:24 +0100

    removed table from viewrecipes

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const ejs = require('ejs');
-const { kStringMaxLength } = require('buffer');
+
 
 app.set('view engine', 'ejs');
 
 
 mongoose.connect("mongodb+srv://dthomas1986:r00tuser86@firstcluster.pf6q6.mongodb.net/new_recipe_set?retryWrites=true&w=majority");
 
-const recipesSchema = {
+const recipeSchema = {
     title: String,
     ingredients: Array,
     instructions: String,
-    picture_link: String
 }
 
-const Recipe = mongoose.model('Recipe', recipesSchema);
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
 app.use(express.static('public'));
 
